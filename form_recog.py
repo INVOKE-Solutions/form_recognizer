82b03088994b0fb6e9f2a24b46e3ee083c75a0f9
Ammar-Azman <muhammarsafuan@gmail.com>
2023-05-05 10:39:20 +0800

    feat: Add get_basic_info and display_basic_info function

--- form_recog.py
+++ form_recog.py
@@ -14,6 +14,9 @@ def format_polygon(polygon):
 
 
 def format_bounding_region(bounding_region):
+    """
+    Return bounding box region recognize within the pdf file. 
+    """
     if not bounding_region:
         return "N/A"
     return ", ".join("Page #{}: {}".format(
@@ -25,6 +28,17 @@ def analyze_document(doc_is_url=False,
                     docURL:str=False, 
                     docPath:str=False, 
                     prebuilt_model:str="prebuilt-document"):
+    
+    """
+    Return azure.ai.formrecognizer._models.AnalyzedDocument 
+
+    *Args
+        doc_is_url: True if document is from URL (ie Github link)
+        docURL    : Document URL link if doc_is_url is True
+        docPath   : Document path in local (doc_is_url either False or no args)
+        prebuilt_model: Pre-built model from Form Recognizer (default is prebuild-document; 
+                        alternative; invoice-document)
+    """
 
     doc_analysis_client = DocumentAnalysisClient(
                             endpoint=os.getenv("endpoint"),
@@ -42,8 +56,12 @@ def analyze_document(doc_is_url=False,
     result = poller.result()
     return result
 
-
 def key_val_extraction(result:azure.ai.formrecognizer._models.AnalyzeResult)->Dict:
+    """
+    Return raw parsed information without classification from the document
+    
+    """
+
     information = {
                 "key-content":[],
                 "key-bbox":[], 
@@ -84,3 +102,67 @@ def key_val_extraction(result:azure.ai.formrecognizer._models.AnalyzeResult)->Di
 
     # return pd.DataFrame(information).head(5)
     return information
+
+def get_basic_info(invoice:azure.ai.formrecognizer._models.AnalyzedDocument, 
+                    infoToget:str):
+    
+    """
+    Return basic information from the document
+    """
+    
+    getInfo = invoice.fields.get(infoToget)
+
+    if getInfo:
+        if infoToget == "CustomerAddress" or \
+            infoToget == "ShippingAddress" or \
+                infoToget == "VendorAddress" :
+            # print(f"{infoToget}: {getInfo.value.city} | {infoToget} confidence: {getInfo.confidence}")
+            return (str(getInfo.value.city), str(getInfo.confidence))
+        else:
+            # print(f"{infoToget}: {getInfo.value} | {infoToget} confidence: {getInfo.confidence}")
+            return (str(getInfo.value), str(getInfo.confidence))
+
+def display_basic_info(invoice_result:azure.ai.formrecognizer._models.AnalyzeResult):
+
+    """
+    Return dictionary that has key-value of attribute-value from the docs 
+
+    """
+
+    for idx, invoice_result in enumerate(invoice_result.documents):
+        print(f"----- Invoice # {idx+1}")
+
+        get_this = [
+            "VendorName", 
+            "VendorAddress"
+            "VendorAddressRecipient", 
+            "CustomerName", 
+            "CustomerId", 
+            "CustomerAddress", 
+            "CustomerAddressRecipient", 
+            "InvoiceId", 
+            "InvoiceDate", 
+            "InvoiceTotal", 
+            "DueDate", 
+            "PurchaseOrder", 
+            "BillingAddress"
+            "BillingAddressRecipient", 
+            "ShippingAddress", 
+            "ShippingAddressRecipient", 
+        ]
+        infoDict = {"Attribute":[], "Value":[], "Conf":[]}
+
+        for content in get_this:
+            info = get_basic_info(invoice_result, content)
+            if info == None:
+                    infoDict["Attribute"].append(np.NAN)
+                    infoDict["Value"].append(np.NAN)
+                    infoDict["Conf"].append(np.NAN)
+            else:
+                val = get_basic_info(invoice=invoice_result, infoToget=content)[0]
+                conf = get_basic_info(invoice=invoice_result, infoToget=content)[1]
+                infoDict["Attribute"].append(content)
+                infoDict["Value"].append(val)
+                infoDict["Conf"].append(conf)
+
+        return infoDict

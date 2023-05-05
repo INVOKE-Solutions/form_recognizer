5a5df9036f8f9598f082abcd1a9eea778a0a1413
Ammar-Azman <muhammarsafuan@gmail.com>
2023-05-05 09:29:41 +0800

    feat: Add configure function for secret endpoint and key string config

--- utils.py
+++ utils.py
@@ -1,13 +1,4 @@
-def format_polygon(polygon):
-    if not polygon:
-        return "N/A"
-    return ", ".join("[{}, {}]".format(p.x, p.y) for p in polygon)
+from dotenv import load_dotenv
 
-
-def format_bounding_region(bounding_region):
-    if not bounding_region:
-        return "N/A"
-    return ", ".join("Page #{}: {}".format(
-                    region.page_number, format_polygon(region.polygon)
-                    )for region in bounding_region
-                    )
+def configure():
+    load_dotenv()

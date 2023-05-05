2127e745ade6f56162649cbd4a97d9bd9be1bb8e
Ammar-Azman <muhammarsafuan@gmail.com>
2023-05-05 12:58:41 +0800

    feat: Added test_basic_info and test_desc_info

--- unit_test/test_fr.py
+++ unit_test/test_fr.py
@@ -1,21 +1,33 @@
 from main_project.utils import configure
 from main_project.form_recog import analyze_document, \
-                                        display_basic_info
+                                    display_basic_info, \
+                                    display_item_description
 
-def test_basic_info():
+def analyze_doc():
     url_pdf = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-REST-api-samples/master/curl/form-recognizer/sample-invoice.pdf"
     configure()
     print("Recognition started...")
     document_result = analyze_document(
-        doc_is_url=False, 
+        doc_is_url=True, 
         docURL=url_pdf, 
         prebuilt_model="prebuilt-invoice", 
         )
-    
+    return document_result
+
+def test_basic_info():
+    document_result = analyze_doc()
     basic_information = display_basic_info(document_result)
     
     assert len(basic_information["Attribute"]) == \
                 len(basic_information["Value"]) == \
                     len(basic_information["Conf"]), "Len list is not same"
+
+def test_desc_info():
+    document_result = analyze_doc()
+    desc_information = display_item_description(document_result)
+    
+    assert len(desc_information["Attribute"]) == \
+                len(desc_information["Value"]) == \
+                    len(desc_information["Conf"]), "Len list is not same"
 
     

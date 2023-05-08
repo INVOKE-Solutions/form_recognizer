c311ba75bcd7ba9d08e3b5292e04134c199bac8f
Ammar-Azman <muhammarsafuan@gmail.com>
2023-05-08 12:27:34 +0800

    feat: Added display_pdf_to_image; using pdf2image

--- streamlitui/utils.py
+++ streamlitui/utils.py
@@ -1,7 +1,17 @@
 import streamlit as st
 import base64
+from pdf2image import convert_from_bytes
 
 def displaypdf(file: st.runtime.uploaded_file_manager.UploadedFile):
+    """
+    BUG: Not working on the cloud. Bug on Streamlit side.
+    
+    """
     bytes_pdf = base64.b64encode(file.read()).decode("utf-8")
     pdf_display = f'<embed src="data:application/pdf;base64,{bytes_pdf}" width="600" height="400" type="application/pdf">'
     st.markdown(pdf_display, unsafe_allow_html=True)
+
+def display_pdf_to_image(file:st.runtime.uploaded_file_manager.UploadedFile):
+    bytes_pdf = base64.b64encode(file.read()).decode("utf-8")
+    image = convert_from_bytes(bytes_pdf, 500)
+    return st.image(image)

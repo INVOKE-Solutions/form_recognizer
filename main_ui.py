a929ebbc866c5c88e9b865ce8ca02d7fdc1be1e0
Ammar-Azman <muhammarsafuan@gmail.com>
2023-05-08 16:54:40 +0800

    Fix: Converted doc to bytes by adding .getvalue()

--- main_ui.py
+++ main_ui.py
@@ -3,8 +3,9 @@ from streamlitui.fr_ui import sidebar, parse_button, display_df
 from streamlitui.utils import displaypdf, display_pdf_to_image
 
 import os, sys
+from io import StringIO
 # sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "main_project"))
-from main_project.main import main
+from main_project.main import recognize_this
 
 
 def main_streamlit():
@@ -22,14 +23,17 @@ def main_streamlit():
                 with st.expander("See PDF"):
                     display_pdf_to_image(file=doc)
 
-        # PARSING PROCESS
-        if parseButton:
-            with st.spinner("Parsing..."):
-                parseInfo = main()
-                
-            with tab2:
-                display_df(parseInfo[0])
-                display_df(parseInfo[1])
+            # PARSING PROCESS
+            if parseButton:
+                with st.spinner("Parsing..."):
+                    parseInfo = recognize_this(
+                        doc_is_url=False, 
+                        doc_path=doc.getvalue()
+                    )
+                        
+                    with tab2:
+                        display_df(parseInfo[0])
+                        display_df(parseInfo[1])
     else:
         st.warning("No PDF uploaded.")
 

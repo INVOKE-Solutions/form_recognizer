2355858effea850106716bbe01eef351136da14e
Ammar-Azman <muhammarsafuan@gmail.com>
2023-05-08 12:29:59 +0800

    applied display_pdf_to_image function

--- streamlitui/main_ui.py
+++ streamlitui/main_ui.py
@@ -1,12 +1,13 @@
 from fr_ui import sidebar, parse_button
-
-from utils import displaypdf
+from utils import displaypdf, display_pdf_to_image
 import streamlit as st
 def main():
     uploaded_pdf = sidebar()
     if uploaded_pdf:
         for idx, doc in enumerate(uploaded_pdf):
-            displaypdf(file=doc)
+            # displaypdf(file=doc)
+            st.write(f"PDF name: {doc.name}")
+            display_pdf_to_image(file=doc)
         # parse_button()
 
 

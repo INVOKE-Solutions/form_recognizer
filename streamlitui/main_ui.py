65717e57760eda6ec2c20a8c0d509c257cce418c
Ammar-Azman <muhammarsafuan@gmail.com>
2023-05-08 11:48:56 +0800

    feat: Added displaypdf()

--- streamlitui/main_ui.py
+++ streamlitui/main_ui.py
@@ -1,12 +1,14 @@
-from fr_ui import (
-    sidebar,
-    main_page
-)
+from fr_ui import sidebar, parse_button
 
+from utils import displaypdf
+import streamlit as st
 def main():
     uploaded_pdf = sidebar()
     if uploaded_pdf:
-        main_page()
+        for idx, doc in enumerate(uploaded_pdf):
+            displaypdf(file=doc)
+        # parse_button()
+
 
 if __name__ == "__main__":
     main()

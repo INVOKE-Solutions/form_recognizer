abd136f18b153a70dd020bf7f3367b85ad39ee34
Ammar-Azman <muhammarsafuan@gmail.com>
2023-05-08 12:05:02 +0800

    Edit iframe to embed

--- streamlitui/utils.py
+++ streamlitui/utils.py
@@ -3,5 +3,5 @@ import base64
 
 def displaypdf(file: st.runtime.uploaded_file_manager.UploadedFile):
     bytes_pdf = base64.b64encode(file.read()).decode("utf-8")
-    pdf_display = f'<iframe src="data:application/pdf;base64,{bytes_pdf}" width="600" height="400" type="application/pdf"></iframe>'
+    pdf_display = f'<embed src="data:application/pdf;base64,{bytes_pdf}" width="600" height="400" type="application/pdf">'
     st.markdown(pdf_display, unsafe_allow_html=True)

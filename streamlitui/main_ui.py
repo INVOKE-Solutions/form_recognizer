7640fc6ff27bd7ace672dc54368c0bdcfcc81aba
Ammar-Azman <muhammarsafuan@gmail.com>
2023-05-08 13:42:44 +0800

    feat: Addded display_pdf_to_image()

--- streamlitui/main_ui.py
+++ streamlitui/main_ui.py
@@ -7,8 +7,9 @@ def main():
         for idx, doc in enumerate(uploaded_pdf):
             # displaypdf(file=doc)
             st.write(f"PDF name: {doc.name}")
-            display_pdf_to_image(file=doc)
-        # parse_button()
+            with st.expander("See PDF"):
+                display_pdf_to_image(file=doc)
+        parse_button()
 
 
 if __name__ == "__main__":

059bcc319c7aa83bbe392f4b21de64c0cf392333
Ammar-Azman <muhammarsafuan@gmail.com>
2023-05-08 11:49:39 +0800

    fix: Debug displaypdf() to read UploadedFile format

---
+++ streamlitui/utils.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import base64
+
+def displaypdf(file: st.runtime.uploaded_file_manager.UploadedFile):
+    bytes_pdf = base64.b64encode(file.read()).decode("utf-8")
+    pdf_display = f'<iframe src="data:application/pdf;base64,{bytes_pdf}" width="600" height="400" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)

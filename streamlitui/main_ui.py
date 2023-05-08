0c76c0a6518c346a50851c51082044ac1348798e
Ammar-Azman <muhammarsafuan@gmail.com>
2023-05-08 14:36:18 +0800

    feat: Add tab and flow to the UI

--- streamlitui/main_ui.py
+++ streamlitui/main_ui.py
@@ -1,22 +1,34 @@
 import streamlit as st
-from fr_ui import sidebar, parse_button
+from fr_ui import sidebar, parse_button, display_df
 from utils import displaypdf, display_pdf_to_image
 from main_project.main import main
 
 def main_streamlit():
+    # SETUP SIDEBAR & UPLOAD PART
     uploaded_pdf = sidebar()
+
+    # SETUP TAB
+    tab1, tab2 = st.tabs(["PDF Uploaded", "Data Parsed"])
+    # UPLOAD PROCESS
     if uploaded_pdf:
+        parseButton = parse_button()
         for idx, doc in enumerate(uploaded_pdf):
             # displaypdf(file=doc)
             st.write(f"PDF name: {doc.name}")
-            with st.expander("See PDF"):
-                display_pdf_to_image(file=doc)
+            with tab1:
+                with st.expander("See PDF"):
+                    display_pdf_to_image(file=doc)
 
-        parseButton = parse_button()
+        # PARSING PROCESS
         if parseButton:
             with st.spinner("Parsing..."):
-                main()
-
+                parseInfo = main()
+                
+            with tab2:
+                display_df(parseInfo[0])
+                display_df(parseInfo[1])
+    else:
+        st.warning("No PDF uploaded.")
 
 if __name__ == "__main__":
     main_streamlit()

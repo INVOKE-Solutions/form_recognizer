0139fe436ab1b0bbb5d7648c33dc3294bd1da81e
Ammar-Azman <muhammarsafuan@gmail.com>
2023-05-17 17:03:48 +0800

    update status message

--- main_ui.py
+++ main_ui.py
@@ -73,7 +73,7 @@ def main_streamlit():
                             doc_path=doc.getvalue()
                         )
                         st.session_state["parseInfo"].append(parseInfo)
-                    status_message.success("Parsing complete. Click Data Parsed tab.")
+                    status_message.success("Parsing complete. Click Save Document to save to database.")
 
                 except KeyError:
                     status_message.error("You are not authorized to perform this action")
@@ -104,8 +104,6 @@ def main_streamlit():
                         pdf = pdf.replace(["None", "none", "", "False"], np.NAN)
                         st.session_state[f"pdf{idx}"] = pdf
 
-        # else:
-        #     status_message.warning("PDF is not parsed yet.")
 
         # Saving extracted document data to database
         if st.session_state.get("parse_submitbutton", False):

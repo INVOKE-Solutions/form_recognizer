efb393abfbb74aeab2d2d856bf3fece94b12520e
AbdulInvoke <abdulhafiiz@invokeisdata.com>
2023-05-30 14:04:24 +0800

    changed javascript code from cellstyle to rowstyle

--- streamlitui/utils.py
+++ streamlitui/utils.py
@@ -23,13 +23,9 @@ def display_image_cached(file:st.runtime.uploaded_file_manager.UploadedFile):
     return images
 
 def confidence_format(df):
-    if df.iloc[5, 1]:
-        df.iloc[5, 2] = 1.0
-    else:
-        df.iloc[5, 2] = 0.0
     gb = GridOptionsBuilder.from_dataframe(df)
 
-    cellsytle_jscode = JsCode("""
+    jscode = JsCode("""
     function(params) {
         var positive_color = '#40BF60'; //'#009E73';
         var negative_color = '#9A0E2A'; //'#D55E00';
@@ -77,10 +73,12 @@ def confidence_format(df):
         };
     }
     """)
-    gb.configure_columns(df, cellStyle=cellsytle_jscode)
+
     gb.configure_columns("Value", editable=True)
     grid_options = gb.build()
+    grid_options['getRowStyle'] = jscode
     grid_return = AgGrid(df, gridOptions=grid_options, allow_unsafe_jscode=True)
+    print(grid_options.keys())
 
     return grid_return
 

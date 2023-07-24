f29c96fe06461661164924f4a42c17a95a703a8d
AbdulInvoke <abdulhafiiz@invokeisdata.com>
2023-07-24 12:35:55 +0800

    fixed bug where confidence took on 'None' value

--- main_ui.py
+++ main_ui.py
@@ -156,6 +156,14 @@ def main_streamlit():
                             amount_df = table_df[
                                 table_df["Attribute"] == "Amount"
                             ].reset_index(drop=True)
+                            amount_df.replace(
+                                {"Conf": {"None": 0.0}}, 
+                                inplace=True
+                            )
+                            description_df.replace(
+                                {"Conf": {"None": 0.0}}, 
+                                inplace=True
+                            )
                             combined_conf = (
                                 amount_df["Conf"].astype(float)
                                 + description_df["Conf"].astype(float)
